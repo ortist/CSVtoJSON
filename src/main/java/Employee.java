46db865c775219bee9961b91af0a3b40ab8b2aac
Aleksey Panov <panov.aleksey@gmail.com>
2023-01-22 19:43:53 +0300

    Bugs fixed

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -16,4 +16,9 @@ public class Employee {
         this.country = country;
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Employee{id=" + this.id + ", firstName='" + this.firstName + "', lastName='" + this.lastName + "', country='" + this.country + "', age=" + this.age + "}";
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,43 +2,97 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, ParseException {
 
+        // ЗАДАЧА 1. КОНВЕРТАЦИЯ ИЗ CSV В JSON
 
-        // Создаем файл csv
-        String fileName = "data.csv"; // имя файла
-        // Создаем записи
-        String[] employee01 = "1,David,Miller,Australia,30".split(",");
-        String[] employee02 = "2,Ivan,Petrov,RU,23".split(",");
-        // Создаем экземпляр CSVWriter
-        try (CSVWriter writer = new CSVWriter(new FileWriter(fileName))) {
-            // Записываем запись в файл csv
-            writer.writeNext(employee01);
-            writer.writeNext(employee02);
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        String fileName = "data.csv";
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         // Получим список сотрудников из файла csv
         List<Employee> list = parseCSV(columnMapping, fileName);
         // Конвертируем список в формат json
         String json = listToJson(list);
         // Записываем строку формата json в файл
-        writeString(json);
+        writeString(json, "dataFromCsv.json");
+
+        // ЗАДАЧА 2. КОНВЕРТАЦИЯ ИЗ XML В JSON
+
+        // Получим список сотрудников из файла csv
+        list = parseXML("data.xml");
+        //parseXML("data.xml");
+        // Конвертируем список в формат json
+        json = listToJson(list);
+        // Записываем строку формата json в файл
+        writeString(json, "dataFromXml.json");
+
+        //ЗАДАЧА 3. JSON ПАРСЕР
+
+        //json = readString("dataFromCsv.json");              // так не работает
+        //json = readString("dataFromPresentation.json");     // так работает
+        String json_ = "[{\"id\":1,\"firstName\":\"David\",\"lastName\":\"Miller\",\"country\":\"Australia\",\"age\":30},{\"id\":2,\"firstName\":\"Ivan\",\"lastName\":\"Petrov\",\"country\":\"RU\",\"age\":23}]";
+        list = jsonToList(json_);      // не удается досюда дойти, поэтому метод построен на строке, взятой из json файла
+    }
+
+    // Чтение из json в list
+    private static List<Employee> jsonToList(String jsonText) throws ParseException {
+
+
+        Employee employee;
+        List<Employee> list = new ArrayList<Employee>();
+
+        JSONParser parser = new JSONParser(); //1
+        Object obj = parser.parse(jsonText);    //2
+        JSONArray jsonArray = (JSONArray) obj;  //3
+
+        //Type listType = new TypeToken<List<Employee>>() {}.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        //String json = gson.toJson(list, listType);
+
+        for (int i = 0; i < jsonArray.size(); i++) {
+            employee = gson.fromJson(jsonArray.get(i).toString(), Employee.class);
+            list.add(employee);
+            // вывод элементов списка в консоль
+            System.out.println(employee.toString());
+        }
+
+        return list;
+    }
+
+    // Чтение из файла json
+    private static String readString(String fileName) {
+        JSONParser parser = new JSONParser();
+        //JSONObject jsonObject = null;
+        try {
+            Object obj = parser.parse(new FileReader(fileName));
+            JSONObject jsonObject = (JSONObject) obj;
+            return jsonObject.toJSONString();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     // Получение списка из файла csv
@@ -61,6 +115,30 @@ public class Main {
         return staff;
     }
 
+    // Получение списка из файла XML
+    public static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+        Node root = doc.getDocumentElement();
+        List<Employee> list = new ArrayList<Employee>();
+        NodeList nodeList = root.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node_ = nodeList.item(i);
+            if (node_.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) node_;
+                list.add(new Employee(
+                        Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent()),
+                        element.getElementsByTagName("firstName").item(0).getTextContent(),
+                        element.getElementsByTagName("lastName").item(0).getTextContent(),
+                        element.getElementsByTagName("country").item(0).getTextContent(),
+                        Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent())
+                ));
+            }
+        }
+        return list;
+    }
+
     // Конвертация списка в json
     public static String listToJson(List<Employee> list) {
         Type listType = new TypeToken<List<Employee>>() {}.getType();
@@ -71,14 +149,16 @@ public class Main {
     }
 
     // Запись json в файл
-    public static void writeString(String list) {
+    public static void writeString(String list, String fileName) {
 
         try (FileWriter file = new
-                FileWriter("data.json")) {
+                FileWriter(fileName)) {
             file.write(list);
             file.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
 }//end main
